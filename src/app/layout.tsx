903bc924d3debe8e196ce06c5874a933d699c245
André Silveira <andremarcia4@gmail.com>
2024-06-01 03:11:03 -0400

    feat: add analytics & speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import ClientRootLayoutWrapper from "@/components/ClientRootLayoutWrapper";
-import "./globals.css";
 import TopNavbar from "@/components/TopNavbar";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +20,8 @@ export default function RootLayout({
 }>) {
   return (
     <ClientRootLayoutWrapper>
+      <Analytics />
+      <SpeedInsights />
       <html lang="pt-BR">
         <body className={`${inter.className} bg-custom-blue-50 pt-4`}>
           {/* <TopNavbar /> */}
